41f6f9156ede5cba978e251c92f4fb5019047e72
Yonny <yonnyriveraespinoza@gmail.com>
2024-11-30 17:17:38 -0500

    Initial Commit

---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Yonny on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
